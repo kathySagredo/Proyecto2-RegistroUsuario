19f2bd4beb1bfbe90163ed06bcc7921c5f1d45b0
Kathy Sagredo <sagredo.kathy@gmail.com>
2024-06-11 12:56:59 -0400

    Interfaz de registro básica terminada

--- registro-usuarios-react/src/App.jsx
+++ registro-usuarios-react/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Admin from './components/admin/Admin';
+import UserRegister from './components/user-register/UserRegister';
+import UserLogin from './components/user-login/UserLogin';
+
+/* import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';: Aquí estamos importando varias cosas de una biblioteca llamada react-router-dom. Esta biblioteca se usa para manejar la navegación entre diferentes "páginas" dentro de una aplicación React. */
+/* BrowserRouter (renombrado como Router): Imagina que es el GPS de tu aplicación. Te ayuda a moverte entre diferentes lugares (o páginas). */
+/* Route: Es como una señal de tráfico que te dice a dónde ir cuando tomas una ruta específica. */
+/* Routes: Es un contenedor para todas las rutas (señales de tráfico) que defines. */
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router> {/* <Router>: Esto envuelve toda tu aplicación y permite que uses la navegación. Es como si estuvieras diciendo "a partir de aquí, puedo moverme entre diferentes páginas". */} 
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/registro" element={<UserRegister />} />
+        <Route path="/login" element={<UserLogin />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- registro-usuarios-react/src/main.jsx
+++ registro-usuarios-react/src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/routes";
+import App from './App'
 
 /* ReactDOM.createRoot se utiliza para crear un "punto de entrada" en el HTML donde se mostrará tu aplicación React. */
 /* document.getElementById('root') busca un elemento en tu archivo HTML con el ID root, que es donde tu aplicación será renderizada (mostrada). */
 ReactDOM.createRoot(document.getElementById('root')).render( /* .render(...): Este método render se usa para mostrar lo que se pasa dentro de él en el navegador. */
   <React.StrictMode> 
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
 /* <React.StrictMode>: Este es un componente de React que envuelve tu aplicación para ayudarte a identificar problemas potenciales en tu código. No afecta cómo se ve la aplicación, pero ayuda a los desarrolladores a escribir mejores aplicaciones.*/
